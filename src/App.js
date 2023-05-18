864622fada66c9b8567ebb77f86d21d5bf561c28
patricio.g <patricio.g@strivelabs.io>
2023-05-18 19:52:33 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// styles
+import "bootstrap/scss/bootstrap.scss";
+import "assets/scss/paper-kit.scss?v=1.3.0";
+import "assets/demo/demo.css?v=1.3.0";
+// pages
+import Index from "views/Index.js";
+import Narrativa from "views/vistasRocio/narrativa";
+import Niveles from "views/vistasRocio/niveles";
+import RegisterPage from "views/examples/RegisterPage.js";
+import Recompensas from "views/vistasRocio/recompensas";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route
+        path="/narrativa"
+        render={(props) => <Narrativa {...props} />}
+      />
+      <Route
+        path="/niveles"
+        render={(props) => <Niveles {...props} />}
+      />
+      <Route
+        path="/recompensas"
+        render={(props) => <Recompensas {...props} />}
+      />
+      <Route
+        path="/retos"
+        render={(props) => <RegisterPage {...props} />}
+      />
+    </Switch>
+  </BrowserRouter>
   );
 }
 
