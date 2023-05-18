4f6e29c81685f15af1a1f09e18b24eb97bb4a479
patricio.g <patricio.g@strivelabs.io>
2023-05-18 20:01:58 +0200

    feat

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // styles
 import "bootstrap/scss/bootstrap.scss";
-import "assets/scss/paper-kit.scss?v=1.3.0";
-import "assets/demo/demo.css?v=1.3.0";
+import "./assets/scss/paper-kit.scss?v=1.3.0";
+import "./assets/demo/demo.css?v=1.3.0";
 // pages
 import Index from "./views/Index.js";
 import Narrativa from "./views/vistasRocio/narrativa";
